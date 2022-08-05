193fcfa5d68a3edcbfcef2e0a058b494d24749fe
cdescobedo <cdescobedo@users.noreply.github.com>
2022-08-05 02:05:38 +0000

    ✅ Ready to clone and code.

--- fastapifromtemplate/config.py
+++ fastapifromtemplate/config.py
@@ -5,11 +5,11 @@ from dynaconf import Dynaconf
 HERE = os.path.dirname(os.path.abspath(__file__))
 
 settings = Dynaconf(
-    envvar_prefix="project_name",
+    envvar_prefix="fastapifromtemplate",
     preload=[os.path.join(HERE, "default.toml")],
     settings_files=["settings.toml", ".secrets.toml"],
     environments=["development", "production", "testing"],
-    env_switcher="project_name_env",
+    env_switcher="fastapifromtemplate_env",
     load_dotenv=False,
 )
 
@@ -18,7 +18,7 @@ settings = Dynaconf(
 # How to use this application settings
 
 ```
-from project_name.config import settings
+from fastapifromtemplate.config import settings
 ```
 
 ## Acessing variables
@@ -45,15 +45,15 @@ KEY=value
 
 ### As environment variables
 ```
-export PROJECT_NAME_KEY=value
-export PROJECT_NAME_KEY="@int 42"
-export PROJECT_NAME_KEY="@jinja {{ this.db.uri }}"
-export PROJECT_NAME_DB__uri="@jinja {{ this.db.uri | replace('db', 'data') }}"
+export fastapifromtemplate_KEY=value
+export fastapifromtemplate_KEY="@int 42"
+export fastapifromtemplate_KEY="@jinja {{ this.db.uri }}"
+export fastapifromtemplate_DB__uri="@jinja {{ this.db.uri | replace('db', 'data') }}"
 ```
 
 ### Switching environments
 ```
-PROJECT_NAME_ENV=production project_name run
+fastapifromtemplate_ENV=production fastapifromtemplate run
 ```
 
 Read more on https://dynaconf.com
